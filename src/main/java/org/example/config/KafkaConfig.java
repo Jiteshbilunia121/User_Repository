5cba9db72d7c28a42c489be356e2f20a422a4f91
jiteshbilunia121 <jiteshbilunia1234@gmail.com>
2025-04-04 23:33:45 +0530

    kafka configuration for creating kafkatemplate Bean

---
+++ src/main/java/org/example/config/KafkaConfig.java
@@ -0,0 +1,41 @@
+package org.example.config;
+
+
+import com.stripe.model.PaymentIntent;
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringDeserializer;
+import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.core.DefaultKafkaProducerFactory;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.core.ProducerFactory;
+import org.springframework.kafka.support.serializer.JsonSerializer;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Configuration
+public class KafkaConfig {
+
+    // ✅ Producer Factory for CheckoutEvent
+    @Bean
+    public ProducerFactory<String, PaymentIntent> eventProducerFactory() {
+        Map<String, Object> configProps = new HashMap<>();
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
+        configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
+        configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "my-consumer-group");
+        configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        return new DefaultKafkaProducerFactory<>(configProps);
+    }
+
+    // ✅ Kafka Template for CheckoutEvent
+    @Bean
+    public KafkaTemplate<String, PaymentIntent> eventKafkaTemplate() {
+        return new KafkaTemplate<>(eventProducerFactory());
+    }
+
+}
